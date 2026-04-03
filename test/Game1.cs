2e2fdd201486786ede5a99ea9e7668163f704e8e
evgesha1999 <evgesha.22013@gmail.com>
2026-04-04 00:30:17 +0300

    Добавьте файлы проекта.

---
+++ test/Game1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
+using MonoGameLibrary.Graphics;
+
+namespace DungeonSlime;
+
+public class Game1 : Core
+{
+    private AnimatedSprite _slime;
+    private AnimatedSprite _bat;
+
+    public Game1() : base("Dungeon Slime", 1280, 720, false)
+    {
+
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        TextureAtlas atlas = TextureAtlas.FromFile(Content, "Images/atlas-defenition.xml");
+
+        _slime = atlas.CreateAnimatedSprite("slime-animation");
+        _slime.Scale = new Vector2(4.0f, 4.0f);
+
+        _bat = atlas.CreateAnimatedSprite("bat-animation");
+        _bat.Scale = new Vector2(4f, 4f);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        _slime.Update(gameTime);
+        _bat.Update(gameTime);
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.Black);
+
+        SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
+
+        _slime.Draw(SpriteBatch, Vector2.Zero);
+        _bat.Draw(SpriteBatch, new Vector2(_slime.Width + 10, 0));
+
+        SpriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
